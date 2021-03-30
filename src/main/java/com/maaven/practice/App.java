d30277e9afaf25a4e10654e851f5ddcd1e926af5
vinodhgoud409 <48838490+vinodhgoud409@users.noreply.github.com>
2021-03-30 13:10:55 +0530

    praveen bro selected  for testyantra.pvt

--- src/main/java/com/maaven/practice/App.java
+++ src/main/java/com/maaven/practice/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello  welcome to praveen bro on board meeting" );
     }
 }
